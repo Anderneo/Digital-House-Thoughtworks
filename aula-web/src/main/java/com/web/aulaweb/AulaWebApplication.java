6f213f83ff4520c14e5fe296eb1f758541c8add9
Anderneo <anderneo.nardi@gmail.com>
2022-07-05 16:14:56 -0300

    removed unused imports

--- aula-web/src/main/java/com/web/aulaweb/AulaWebApplication.java
+++ aula-web/src/main/java/com/web/aulaweb/AulaWebApplication.java
@@ -1,11 +1,5 @@
 package com.web.aulaweb;
 
-import com.web.aulaweb.domain.model.Aluno;
-import com.web.aulaweb.domain.model.Sala;
-import com.web.aulaweb.domain.repository.SalaRepository;
-import com.web.aulaweb.domain.service.SalaService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
